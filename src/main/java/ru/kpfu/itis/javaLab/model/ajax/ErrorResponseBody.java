d709a6e3e801b13b6233ea72aa25ca0dbfbddf09
Ramil Safin <RamilSafNab1996@gmail.com>
2017-06-11 22:43:01 +0300

    Close #13

---
+++ src/main/java/ru/kpfu/itis/javaLab/model/ajax/ErrorResponseBody.java
@@ -0,0 +1,47 @@
+package ru.kpfu.itis.javaLab.model.ajax;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+/**
+ * Created by Safin Ramil on 11.06.17
+ * RamilSafNab1996@gmail.com
+ */
+public class ErrorResponseBody implements Serializable {
+
+    private String message;
+
+    private int code;
+
+    public ErrorResponseBody() {
+
+    }
+
+    public ErrorResponseBody(String message) {
+        this.message = message;
+    }
+
+    public ErrorResponseBody(String message, int code) {
+        this.message = message;
+        this.code = code;
+    }
+
+    @JsonProperty("message")
+    public String getMessage() {
+        return message;
+    }
+
+    @JsonProperty("code")
+    public int getCode() {
+        return code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
--- src/main/java/ru/kpfu/itis/javaLab/service/implementaions/CustomBlogService.java
+++ src/main/java/ru/kpfu/itis/javaLab/service/implementaions/CustomBlogService.java
@@ -7,12 +7,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.kpfu.itis.javaLab.model.ajax.CommentResponseBody;
+import ru.kpfu.itis.javaLab.model.entities.Comment;
 import ru.kpfu.itis.javaLab.model.entities.Post;
 import ru.kpfu.itis.javaLab.model.entities.Tag;
+import ru.kpfu.itis.javaLab.model.entities.User;
+import ru.kpfu.itis.javaLab.repository.spring.CommentRepository;
 import ru.kpfu.itis.javaLab.repository.spring.PostRepository;
 import ru.kpfu.itis.javaLab.repository.spring.TagRepository;
 import ru.kpfu.itis.javaLab.service.interfaces.BlogService;
+import ru.kpfu.itis.javaLab.web.forms.CommentForm;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,11 +34,16 @@ public class CustomBlogService implements BlogService {
 
     private final PostRepository postRepository;
     private final TagRepository tagRepository;
+    private final CommentRepository commentRepository;
 
     @Autowired
-    public CustomBlogService(PostRepository postRepository, TagRepository tagRepository) {
+    public CustomBlogService(
+        PostRepository postRepository, TagRepository tagRepository,
+        CommentRepository commentRepository
+    ) {
         this.postRepository = postRepository;
         this.tagRepository = tagRepository;
+        this.commentRepository = commentRepository;
     }
 
     @Override
@@ -51,6 +62,26 @@ public class CustomBlogService implements BlogService {
     @Transactional(readOnly = true)
     public Optional<Post> getPostById(Long id) {
         return postRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public CommentResponseBody saveComment(User commenter, CommentForm form) {
+
+        Comment comment =commentRepository.save(fromCommentFormAndUser(form, commenter));
+
+        return new CommentResponseBody(comment.getCommentorName(), comment.getCreated(), comment.getContent());
+    }
+
+
+    private Comment fromCommentFormAndUser(CommentForm form, User commenter) {
+        Comment comment = new Comment();
+        comment.setCommenterId(commenter.getId());
+        comment.setCommentorName(commenter.getEmail());
+        comment.setContent(form.getContent());
+        comment.setCreated(LocalDateTime.now());
+        comment.setPostId(form.getPostId());
+        return comment;
     }
 
 }
